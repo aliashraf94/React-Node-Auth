287fa2d0af372bbbbbffb91d3ca544c2a4441be2
Ali Ashraf <adaydreamer10@gmail.com>
2021-07-10 02:21:19 +0200

    setting up the app

---
+++ react-node-auth/src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Switch>
+      
+    </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
