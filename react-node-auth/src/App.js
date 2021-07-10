8986aab5e84122cd883fc43e7d42f8284f8963de
Ali Ashraf <adaydreamer10@gmail.com>
2021-07-10 02:32:56 +0200

    adding components

--- react-node-auth/src/App.js
+++ react-node-auth/src/App.js
@@ -6,12 +6,17 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Home from "./Home"
+import Signup from "./Signup"
+import Login from "./Login"
 
 function App() {
   return (
     <BrowserRouter>
     <Switch>
-      
+      <Route path="/home" exact component={Home} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/login" component={Login} />
     </Switch>
     </BrowserRouter>
   );
---
+++ react-node-auth/src/Signup.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Signup = () => {
+    return(
+        <div>
+            <h1>I Am Signup</h1>
+        </div>
+    )
+}
+
+
+export default Signup
