569b44aa950a0f8cffffb1e7429cbd40651bd541
Ali Ashraf <adaydreamer10@gmail.com>
2021-07-10 02:39:49 +0200

    nav bar

--- react-node-auth/src/App.js
+++ react-node-auth/src/App.js
@@ -13,6 +13,13 @@ import Login from "./Login"
 function App() {
   return (
     <BrowserRouter>
+    <nav>
+      <ul>
+        <li><Link to="/Home" > Home </Link></li>
+        <li><Link to="/signup" > Signup </Link></li>
+        <li><Link to="/login" > Login </Link></li>
+      </ul>
+    </nav>
     <Switch>
       <Route path="/home" exact component={Home} />
       <Route path="/signup" component={Signup} />
